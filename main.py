c3afdac33012eaf2560e14c2112fabda430cd215
AnujTiwari226 <anujkumartiwari.nmims@gmail.com>
2025-03-16 19:34:31 +0530

    new problem statements

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,3 +11,32 @@ def index():
 @app.get('/about')
 def about():
     return {'data': 'About Page'}
+
+@app.get('/blog/unpublished')
+def get_unpublished_blogs():
+    return {'data': 'All the unpublished blogs'}
+
+@app.get('/blog/{blog_id}')
+def show_blogs(blog_id: int):
+    return {'Blog': {'Displaying Blog with id' :blog_id}}
+
+@app.get('/blog')
+def blogs_home(limit: int | None, published: Optional[bool]= True, sort: str | None = None):
+    if published:
+        return {'data': f' Fetching only {limit} blogs from DB where the blogs are published'}
+    
+    return {'data': f' Fetching only {limit} blogs from DB where the blogs are not published'}
+
+# Post methods
+
+class Blog(BaseModel):
+    title: str
+    publisher_name: str
+    published : Optional[bool]
+
+@app.post('/blog')
+def add_new_blog(blog: Blog):
+    return {'data': {"New Entry": f'New Blog has been created with title "{blog.title}"'}}
+
+
+
